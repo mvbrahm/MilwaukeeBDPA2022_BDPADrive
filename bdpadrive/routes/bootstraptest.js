d4e86d10d2bd07968cfb3f233ec95d4aaa7b4d06
mvbrahm <55369617+mvbrahm@users.noreply.github.com>
2022-06-17 05:29:39 -0500

    small changes to boostrap.ejs

--- bdpadrive/routes/bootstraptest.js
+++ bdpadrive/routes/bootstraptest.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('bootstraptest', { title: 'Test' });
+  res.render('bootstraptest', { title: 'Bootstrap Test' });
 });
 
 module.exports = router;
