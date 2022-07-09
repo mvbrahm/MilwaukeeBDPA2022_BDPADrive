eab151e18093de37e6735e6f88fc037e432c2978
matt <matt.brahm@gmail.com>
2022-07-09 10:33:26 -0500

    preliminary explorer views

---
+++ bdpadrive/routes/explorer.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+var apiParameters = require('../configs/api');
+
+// this gets executed whenever localhost:3000/apitest is opened
+router.get('/', function (req, res, next) {
+  res.render('explorer', { title: 'Explorer', nodeData: null });
+});
+
+const user = 'MKEbdpa';
+
+// this gets executed whenever localhost:3000/apitest/ListNodes is opened
+router.get('/ListNodes', async (req,res,next) => {
+  const fetch = require('node-fetch');
+  let myURL = apiParameters.baseurl + "filesystem/" + user + "/search?";
+  const result = await fetch(myURL, {
+    method: 'GET',
+    headers: apiParameters.headers
+  });
+  const responseData = await result.json();
+  console.log(responseData);
+  res.render('explorer', { title: 'Explorer', nodeData: responseData.nodes});
+});
+
+
+module.exports = router;
